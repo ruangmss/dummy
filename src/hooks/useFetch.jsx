b9097208c5377fa190501a8a43414b528f96aef6
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-03 16:47:25 -0300

    feat: Inclui custom hooks para fetch e modificação de descrições

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const request = React.useCallback(async (url, options) => {
+    let response = null;
+    let json = null;
+
+    try {
+      setError('');
+      setData(null);
+      setLoading(true);
+
+      response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(
+          response.status === 404
+            ? 'Recurso não encontrado.'
+            : `Status: ${response.status}. Por favor, entre em contato com a equipe de desenvolvimento.`,
+        );
+      }
+
+      json = await response.json();
+      setData(json);
+    } catch (error) {
+      setError(error.message);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+
+    return { response, json };
+  }, []);
+
+  return { request, data, error, loading };
+};
+
+export default useFetch;
