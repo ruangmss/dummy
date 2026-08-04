e5efe35aa2baad654091d390a36d285d68fc316c
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-04 14:14:48 -0300

    chore: Estrutura rotas iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Button from './components/Button/Button';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Categories from './pages/Categories/Categories';
+import Search from './pages/Search/Search';
+import NotFound from './pages/NotFound/NotFound';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/produtos" element={<Products />} />
+          <Route path="/categorias" element={<Categories />} />
+          <Route path="/pesquisa" element={<Search />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
