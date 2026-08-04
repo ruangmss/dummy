f3862fd585e1e9436b67b8d3e55c6e1a9b386c84
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-04 17:44:50 -0300

    feat: Inclui responsividade parcial no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../assets/icons/logo.svg';
+import menu from '../../assets/icons/menu.svg';
+import search from '../../assets/icons/search.svg';
 import BagIcon from '../../assets/icons/bag.svg?react';
 import UserIcon from '../../assets/icons/user.svg?react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useMatch } from 'react-router-dom';
+import useMedia from '../../hooks/useMedia';
 
 const Header = () => {
+  const mobile = useMedia('(max-width: 768px)');
+  const [mobileNav, setMobileNav] = React.useState(false);
+  const [mobileSearch, setMobileSearch] = React.useState(false);
+
   return (
     <header className="header-bg">
       <div className="header container">
@@ -18,25 +25,57 @@ const Header = () => {
           </span>
         </Link>
 
-        <nav>
-          <ul className="header-list">
-            <NavLink to="/">Início</NavLink>
-            <NavLink to="/produtos">Produtos</NavLink>
-            <NavLink to="/categorias">Categorias</NavLink>
-          </ul>
-        </nav>
+        {!mobile && (
+          <nav>
+            <ul className="header-list">
+              <NavLink to="/">Início</NavLink>
+              <NavLink to="/produtos">Produtos</NavLink>
+              <NavLink to="/categorias">Categorias</NavLink>
+            </ul>
+          </nav>
+        )}
 
-        <input className="header-search" type="text" placeholder="Buscar produtos..." />
+        {!mobile && <input className="header-search" type="text" placeholder="Buscar produtos..." />}
 
         <div className="header-buttons">
+          {mobile && (
+            <button
+              onClick={() => setMobileSearch(!mobileSearch)}
+              className={`header-list-search-icon ${mobileNav ? 'opened' : ''}`}
+            >
+              <img src={search} alt="Ícone de pesquisa" />
+            </button>
+          )}
+
           <NavLink to="/usuario">
             <UserIcon className="header-icon" />
           </NavLink>
           <NavLink to="/sacola">
             <BagIcon className="header-icon" />
           </NavLink>
+
+          {mobile && (
+            <button
+              onClick={() => setMobileNav(!mobileNav)}
+              className={`header-list-mobile-icon ${mobileNav ? 'opened' : ''}`}
+            >
+              <img src={menu} alt="Ícone de menu" />
+            </button>
+          )}
         </div>
       </div>
+
+      {mobile && (
+        <div className={`header-list-mobile-bg ${mobileNav ? 'opened' : ''}`}>
+          <nav>
+            <ul className="header-list-mobile">
+              <NavLink to="/">Início</NavLink>
+              <NavLink to="/produtos">Produtos</NavLink>
+              <NavLink to="/categorias">Categorias</NavLink>
+            </ul>
+          </nav>
+        </div>
+      )}
     </header>
   );
 };
