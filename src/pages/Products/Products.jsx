0b3dc6c2307518d46413f76ad14513f30da82852
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-11 16:23:23 -0300

    feat: Estrutura parte superior da página de produtos

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import Top from './components/Top/Top';
+import { PRODUCTS_GET } from '../../api/api';
+import useFetch from '../../hooks/useFetch';
 
 const Products = () => {
-  return <div>Products</div>;
+  const [query, setQuery] = React.useState('');
+  const [search, setSearch] = React.useState('');
+  const [sort, setSort] = React.useState('');
+  const [order, setOrder] = React.useState('asc');
+  const [page, setPage] = React.useState(1);
+  const { data, request, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchProducts() {
+      const { url, options } = PRODUCTS_GET({ query: search, page: page, sortBy: sort, order: order, page: page });
+      const { json } = await request(url, options);
+
+      console.log(json);
+    }
+
+    fetchProducts();
+  }, [request, page, sort, order, page, search]);
+
+  return (
+    <>
+      <Top
+        search={search}
+        query={query}
+        sort={sort}
+        order={order}
+        setQuery={setQuery}
+        setSort={setSort}
+        setOrder={setOrder}
+        setSearch={setSearch}
+      />
+    </>
+  );
 };
 
 export default Products;
---
+++ src/pages/Products/components/Top/Top.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Top.css';
+import { ToastContext } from '../../../../contexts/ToastContext';
+import Search from '../../../../assets/icons/search.svg?react';
+
+const Top = ({ query, sort, order, setQuery, setSort, setOrder, setSearch }) => {
+  const showToast = React.useContext(ToastContext);
+
+  function separateSort(event) {
+    const [newSort, newOrder] = event.target.value.split('-');
+
+    setSort(newSort);
+    setOrder(newOrder);
+  }
+
+  function searchProduct(event) {
+    event.preventDefault();
+
+    if (query) {
+      setSearch(query.trim());
+    } else {
+      setSearch('');
+      showToast('fail', 'Insira caracteres para efetuar uma pesquisa.');
+    }
+  }
+
+  return (
+    <div className="container top">
+      <h2>Catálogo de Produtos</h2>
+
+      <form className="top-filters" onSubmit={searchProduct}>
+        <div className="top-search">
+          <input
+            placeholder="Busque utilizando uma palavra chave..."
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+
+          <button type="submit" className="top-search-button" aria-label="Buscar produtos">
+            <Search />
+          </button>
+        </div>
+
+        <select value={sort && order ? `${sort}-${order}` : ''} onChange={separateSort}>
+          <option value="">Selecione</option>
+          <option value="price-asc">Menor preço</option>
+          <option value="price-desc">Maior preço</option>
+          <option value="rating-desc">Melhor avaliação</option>
+          <option value="title-asc">Nome: A-Z</option>
+          <option value="title-desc">Nome: Z-A</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default Top;
