be4f84acfd9cf7c85ad41c290cea903d252505e3
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-17 14:23:11 -0300

    feat: Inclui logout

--- src/pages/User/components/Hero/Hero.jsx
+++ src/pages/User/components/Hero/Hero.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import Button from "../../../../components/Button/Button";
-import { Link } from "react-router-dom";
-import "./Hero.css";
+import React from 'react';
+import Button from '../../../../components/Button/Button';
+import { Link } from 'react-router-dom';
+import './Hero.css';
+import { ToastContext } from '../../../../contexts/ToastContext';
 
-const Hero = ({ data }) => {
+const Hero = ({ data, userLogout }) => {
   const initials = [data?.firstName, data?.lastName]
     .filter(Boolean)
     .map((name) => name.charAt(0))
-    .join("")
+    .join('')
     .toUpperCase();
+
+  const showToast = React.useContext(ToastContext);
+
+  function logout() {
+    userLogout();
+    showToast('success', 'Logout executado com sucesso!');
+  }
 
   return (
     <section className="container">
       <div className="user-hero">
         <div className="user-hero-content">
-          <div className="user-hero-avatar">{initials || "U"}</div>
+          <div className="user-hero-avatar">{initials || 'U'}</div>
 
           <div className="user-hero-texts">
             <span>Minha conta</span>
@@ -29,7 +37,7 @@ const Hero = ({ data }) => {
           <Link to="/edicao-perfil" className="button">
             Editar Perfil
           </Link>
-          <Button text="Sair" type="secondary" />
+          <Button text="Sair" type="secondary" onClick={logout} />
         </div>
       </div>
     </section>
