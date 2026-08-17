36d03354ddcc97df961e306638468aef7a96ae86
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-17 14:27:39 -0300

    fix: Impede usuário não logado de acessar rota

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { UserContext } from '../../contexts/UserContext';
 import Hero from './components/Hero/Hero';
 import Data from './components/Data/Data';
+import Error from '../../components/Error/Error';
 
 const User = () => {
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data, userLogout, login } = React.useContext(UserContext);
+
+  if (!login) {
+    return <Error error={'Você não está logado. Por favor, efetue o login para acessar esta rota.'} />;
+  }
 
   return (
     <>
