e636879fb6607f0e30a1c97207be1f1787263bc6
Ruan Gomes <ruan.gmss@outlook.com>
2026-08-18 17:41:01 -0300

    feat: Inclui novos regex

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -7,14 +7,64 @@ const types = {
   },
 
   password: {
-    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9\s]).{8,}$/,
     message:
-      'Por favor, insira uma senha forte: caracteres minúsculos e maiúsculos, caracteres especiais, dígitos e ao menos 8 caracteres.',
+      'Por favor, insira uma senha forte: caracteres minúsculos e maiúsculos, caractere especial, dígito e ao menos 8 caracteres.',
   },
 
   user: {
     regex: /^\S+$/,
     message: 'Por favor, remova os espaços inseridos.',
+  },
+
+  fullName: {
+    regex: /^\p{L}+(?:[ '-]\p{L}+)+$/u,
+    message: 'Por favor, insira o nome e o sobrenome.',
+  },
+
+  phone: {
+    regex: /^(?:\([1-9]{2}\)|[1-9]{2})\s?(?:9\d{4}|\d{4})-?\d{4}$/,
+    message: 'Por favor, insira um telefone válido com DDD.',
+  },
+
+  street: {
+    regex: /^(?=.{3,100}$)(?=.*\p{L})[\p{L}\d\s.,ºª°'#/-]+$/u,
+    message: 'Por favor, insira um logradouro válido.',
+  },
+
+  city: {
+    regex: /^\p{L}+(?:[ '-]\p{L}+)*$/u,
+    message: 'Por favor, insira uma cidade válida.',
+  },
+
+  state: {
+    regex: /^(?:AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)$/i,
+    message: 'Por favor, insira uma UF válida. Exemplo: SP.',
+  },
+
+  postalCode: {
+    regex: /^\d{5}-?\d{3}$/,
+    message: 'Por favor, insira um CEP válido.',
+  },
+
+  cardNumber: {
+    regex: /^(?:\d[ -]?){12,18}\d$/,
+    message: 'Por favor, insira um número de cartão válido.',
+  },
+
+  cardName: {
+    regex: /^\p{L}+(?:[ '-]\p{L}+)+$/u,
+    message: 'Por favor, insira o nome completo como está no cartão.',
+  },
+
+  cardExpiration: {
+    regex: /^(?:0[1-9]|1[0-2])\/\d{2}$/,
+    message: 'Por favor, insira a validade no formato MM/AA.',
+  },
+
+  cardCvv: {
+    regex: /^\d{3,4}$/,
+    message: 'Por favor, insira um CVV válido.',
   },
 };
 
